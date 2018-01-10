b927c32f9c24843bcfb1d6fdf29e7c2c2d7ad046
Vikas Pal <pal.vikas210@gmail.com>
2018-01-10 19:42:59 +0530

    Added code to check UserInfo.class runs once.

--- app/src/main/java/com/example/cas/stepcounter/MainActivity.java
+++ app/src/main/java/com/example/cas/stepcounter/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.cas.stepcounter;
 
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  {
 
-    TabLayout tabLayout;
+    private TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +26,24 @@ public class MainActivity extends AppCompatActivity  {
         tabLayout=(TabLayout)findViewById(R.id.tablayout);
         tabLayout.setupWithViewPager(viewPager);
         setupTabIcons();
+
+        //code to check if UserInfo have run once if not runs UserInfo.class then MainActivity.class
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .getBoolean("isFirstRun", true);
+
+        if (isFirstRun) {
+            startActivity(new Intent(MainActivity.this, UserInfo.class));
+            Toast.makeText(MainActivity.this, "First Run", Toast.LENGTH_LONG)
+                    .show();
+        }
+        getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit()
+                .putBoolean("isFirstRun", false).commit();
     }
+
+    //setting tab icons for images...
     private void setupTabIcons() {
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_home_white_48dp);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_home_white_48dp);
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_home_white_48dp);
     }
-
 }

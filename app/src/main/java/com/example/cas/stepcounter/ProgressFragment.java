3f26996c8aedc960207514c7dcdce444697e8679
Vikas Pal <pal.vikas210@gmail.com>
2018-01-15 23:54:23 +0530

    Added statistics code.

--- app/src/main/java/com/example/cas/stepcounter/ProgressFragment.java
+++ app/src/main/java/com/example/cas/stepcounter/ProgressFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -41,26 +42,31 @@ public class ProgressFragment extends Fragment {
         bmiTV = (TextView) view.findViewById(R.id.bmi_textView);
         bmiCategoryTV = (TextView) view.findViewById(R.id.bmi_category_textView);
 
-        //initialize weight,height,age.
-        mWeight = Float.valueOf(UserInfo.getDefaults(UserInfo.WEIGHT, getContext()));
-        mAge = Float.valueOf(UserInfo.getDefaults(UserInfo.AGE, getContext()));
-        mHeight = Float.valueOf(UserInfo.getDefaults(UserInfo.HEIGHT, getContext()));
 
-        //calculate water
-        waterIntakeValue = dailyWaterIntakeCalculator(mWeight, mAge);
-        waterTextView.setText(String.valueOf(String.format("%.2f", waterIntakeValue)) + " Liters");
-
-        //calculating BMI
-        bmi = calculateBMI(mWeight, mHeight);
-        bmiTV.setText(String.valueOf(String.format("%.2f", bmi)));
-        if (bmi < 18.5) {
-            bmiCategoryTV.setText("UnderWeight");
-        } else if (18.5 <= bmi && bmi <= 24.9) {
-            bmiCategoryTV.setText("Normal Weight");
-        } else if (25 <= bmi && bmi <= 29.9) {
-            bmiCategoryTV.setText("Overweight");
+        if (MainActivity.isFirstRun) {
+            Toast.makeText(getContext(), "Empty", Toast.LENGTH_SHORT).show();
         } else {
-            bmiCategoryTV.setText("Obese");
+            //initialize weight,height,age.
+            mWeight = Float.valueOf(UserInfo.getDefaults(UserInfo.WEIGHT, getContext()));
+            mAge = Float.valueOf(UserInfo.getDefaults(UserInfo.AGE, getContext()));
+            mHeight = Float.valueOf(UserInfo.getDefaults(UserInfo.HEIGHT, getContext()));
+
+            //calculate water
+            waterIntakeValue = dailyWaterIntakeCalculator(mWeight, mAge);
+            waterTextView.setText(String.valueOf(String.format("%.2f", waterIntakeValue)) + " Liters");
+
+            //calculating BMI
+            bmi = calculateBMI(mWeight, mHeight);
+            bmiTV.setText(String.valueOf(String.format("%.2f", bmi)));
+            if (bmi < 18.5) {
+                bmiCategoryTV.setText("UnderWeight");
+            } else if (18.5 <= bmi && bmi <= 24.9) {
+                bmiCategoryTV.setText("Normal Weight");
+            } else if (25 <= bmi && bmi <= 29.9) {
+                bmiCategoryTV.setText("Overweight");
+            } else {
+                bmiCategoryTV.setText("Obese");
+            }
         }
 
         //return view
